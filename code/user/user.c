122119b63b75f251a35a751841d14901058cd25c
Derfies <Derfies@users.noreply.github.com>
2024-08-07 10:41:22 +1000

    no message

--- code/user/user.c
+++ code/user/user.c
@@ -29,7 +29,7 @@ static mp_obj_t user_square(mp_obj_t arg1, mp_obj_t arg2) {
 
     mp_int_t val = 13;
     //uint8_t *rarray = (uint8_t *)ndarray->array;
-    for(size_t i=0; i < ndarray->len; i++, array++) {
+    for(size_t i=0; i < ndarray->len; i++) {
         mp_binary_set_val_array('I', array->items, i, mp_obj_new_int(val));
     }
     //mp_obj_new_int(*rarray++)
